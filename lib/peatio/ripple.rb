9d6d45d12f9ffb70a2c91562697357b77de99fe0
mnaichuk <mnaichuk@heliostech.fr>
2019-05-15 12:56:15 +0300

    Add gem dependencies and edit spec_helper conf

--- lib/peatio/ripple.rb
+++ lib/peatio/ripple.rb
@@ -1,7 +1,9 @@
 require "peatio/ripple/version"
+require "active_support/core_ext/object/blank"
 
 module Peatio
   module Ripple
+    require "peatio/ripple/client"
     class Error < StandardError; end
     # Your code goes here...
   end
