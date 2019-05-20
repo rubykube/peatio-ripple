df0be28c0d9d8acf45a74d5db32665d52e57cb3b
mnaichuk <mnaichuk@heliostech.fr>
2019-05-20 17:39:44 +0300

    Add more requirements for Peatio::Ripple

--- lib/peatio/ripple.rb
+++ lib/peatio/ripple.rb
@@ -1,9 +1,15 @@
 require "peatio/ripple/version"
 require "active_support/core_ext/object/blank"
+require "peatio"
 
 module Peatio
   module Ripple
     require "peatio/ripple/client"
+    require "peatio/ripple/blockchain"
+    require "peatio/ripple/wallet"
+
+    require "peatio/ripple/hooks"
+    require "peatio/ripple/version"
     class Error < StandardError; end
     # Your code goes here...
   end
